1851187d94f964567252c8126312faa40ba0a77e
recedivies <ahmadhiprananta@gmail.com>
2024-04-07 16:54:17 +0700

    Fix Delete API

--- jobs/constants.py
+++ jobs/constants.py
@@ -3,4 +3,3 @@ TASK_HISTORY_NOT_FOUND = "Task History not found"
 
 TASK_SCHEDULE_Z_SET = "task_schedule_z_set"
 FAILED_SET_KEY = "Failed to set key"
-FAILED_DELETE_KEY = "Failed to delete key"
--- jobs/services/delete_job.py
+++ jobs/services/delete_job.py
@@ -1,11 +1,9 @@
 import uuid
 
-from django_redis import get_redis_connection
-
-from commons.exceptions import BadRequestException, NotFoundRequestException
+from commons.exceptions import NotFoundRequestException
 from commons.patterns.runnable import Runnable
 from identities.models import User
-from jobs.constants import FAILED_DELETE_KEY, JOB_NOT_FOUND
+from jobs.constants import JOB_NOT_FOUND
 from jobs.models import Job
 
 
@@ -13,12 +11,7 @@ class DeleteJobService(Runnable):
     @classmethod
     def run(cls, user: User, job_id: uuid.UUID) -> None:
         try:
-            redis_connection = get_redis_connection()
             job = Job.objects.get(id=job_id)
-
-            ok = redis_connection.delete(str(job.id))
-            if not ok:
-                raise BadRequestException(FAILED_DELETE_KEY)
             job.delete()
         except Job.DoesNotExist:
             raise NotFoundRequestException(JOB_NOT_FOUND)
